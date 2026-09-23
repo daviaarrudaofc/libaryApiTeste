f5b476b50df6036ac1df579e43a085238ed92073
davi arruda <daviarruda092@gmail.com>
2026-09-23 18:02:28 -0300

    mapeando entidade autor e configuração conexao do banco de dados

---
+++ src/main/java/io/github/daviaarrudaofc/libaryAPI/config/DatabaseConfiguration.java
@@ -0,0 +1,53 @@
+package io.github.daviaarrudaofc.libaryAPI.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")// para pegar do properties ou yml
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){ // datasource fraco// prove so uma conexao fraca
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+
+        return ds;
+    }
+    @Bean
+    public DataSource hikariDataSource(){// esse method cria e registra no Spring  um datasource e configura como
+                                        // a aplicação se conecta ao banco e gerencianado um pool de ate 10 conexao
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10);// gerencianado um pool de ate 10 conexao(requisição) RELACIONADA AO BANCO DE DADOS!
+        config.setMinimumIdle(1);
+        config.setPoolName("libary-db-pool:conexão");
+        config.setMaxLifetime(600000);// medida em milisegundos
+        config.setConnectionTimeout(100000); // o tempo que vai tentar para obter a conexao
+        config.setConnectionTestQuery("select 1"); // vai testar se ta conectando com o banco
+
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/main/java/io/github/daviaarrudaofc/libaryAPI/model/Autor.java
@@ -0,0 +1,42 @@
+package io.github.daviaarrudaofc.libaryAPI.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID) // vai ser gerado automaticamente, nao precisa se preoucpar, pq o propio JPA vai gerar
+    private UUID id;
+
+    @Column(name = "nome", length = 100,nullable = false)// nullable = significa que nao pode ser not null=como definimos no banco de dados
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade",length = 50, nullable = false)
+    private String nacionalidade;
+
+    @Deprecated
+    public Autor(){
+        // para uso do fremework
+    }
+
+    public Autor(UUID id, String nome, LocalDate dataNascimento, String nacionalidade) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+    }
+}
