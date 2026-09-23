e34012d9eb79272c392bb2d92e50aa945c8be066
davi arruda <daviarruda092@gmail.com>
2026-09-23 14:23:29 -0300

    Initial commit of project

---
+++ src/test/java/io/github/daviaarrudaofc/libaryAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daviaarrudaofc.libaryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
