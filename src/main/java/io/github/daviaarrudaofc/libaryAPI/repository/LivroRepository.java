31029a024bfb90bf3a901df77511462605951e01
davi arruda <daviarruda092@gmail.com>
2026-09-25 12:01:00 -0300

    trabalhando com consultas JPQL e @Query e variação de methods de buscas declarat

--- src/main/java/io/github/daviaarrudaofc/libaryAPI/repository/LivroRepository.java
+++ src/main/java/io/github/daviaarrudaofc/libaryAPI/repository/LivroRepository.java
@@ -3,13 +3,57 @@ package io.github.daviaarrudaofc.libaryAPI.repository;
 import io.github.daviaarrudaofc.libaryAPI.model.Autor;
 import io.github.daviaarrudaofc.libaryAPI.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+/**
+ * @see LivroRepositoryTest
+ */
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
     //QUERY METHOD
     //select * from livro where id_autor = id
     List<Livro> findByAutor(Autor autor);
+    //select * from livro where titulo = titulo_passado_parametro
+    List<Livro> findByTitulo(String titulo);
+    // fiz aq por objeto porque pode ser unico o isbn e nao varios: Livro findByIsbn(String isbn);
+    List<Livro> findByIsbn(String isbn);
+
+    List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
+
+    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
+
+    // select * from livro where data_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
+
+
+    //JPQL -> referencia as entidades e as propriedades
+    @Query(" select l from Livro as l order by l.titulo, l.preco ")
+    List<Livro> listarTodosOrdenadoPorTituloAndPreco();
+
+
+    /**
+     *  select a.*
+     *  from livro as l
+     *  join autor as a on a.id = l.id-autor
+     */
+    @Query(" select a from Livro as l join l.autor a ")
+    List<Autor> listarAutoresDosLivros();
+
+    //select distinct l.* from livro as l
+    @Query(" select distinct l.titulo from Livro as l ")
+    List<String> listarNomesDiferentesLivros();
+
+    @Query("""
+        select l.genero
+        from Livro as l
+        join l.autor a
+        where a.nacionalidade = 'Brasilerio'
+        order by l.genero
+        """)
+    List<String> listarGenerosAutoresBrasileiros();
 
 }
--- src/test/java/io/github/daviaarrudaofc/libaryAPI/repository/LivroRepositoryTest.java
+++ src/test/java/io/github/daviaarrudaofc/libaryAPI/repository/LivroRepositoryTest.java
@@ -3,7 +3,6 @@ package io.github.daviaarrudaofc.libaryAPI.repository;
 import io.github.daviaarrudaofc.libaryAPI.model.Autor;
 import io.github.daviaarrudaofc.libaryAPI.model.GeneroLivro;
 import io.github.daviaarrudaofc.libaryAPI.model.Livro;
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
 public class LivroRepositoryTest {
     @Autowired
-    LivroRepository repository;
+    LivroRepository livroRepository;
 
     @Autowired
     AutorRepository autorRepository;
@@ -38,7 +38,7 @@ public class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
     @Test
     public void salvaAutorELivroTest(){
@@ -58,7 +58,7 @@ public class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
 
 
@@ -78,13 +78,13 @@ public class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        repository.save(livro);
+        livroRepository.save(livro);
     }
 
     @Test
     public void atualizarAutorDoLivro(){
         UUID id = UUID.fromString("c2319c3b-4d51-43c2-bde4-44b2cc315961");
-        var livroParaAtualizar = repository.findById(id)
+        var livroParaAtualizar = livroRepository.findById(id)
                 .orElse(null);
 
         UUID idAutor = UUID.fromString("0fce1d0b-e1ed-43f7-af04-f76e037cba15");
@@ -92,30 +92,77 @@ public class LivroRepositoryTest {
 
         livroParaAtualizar.setAutor(maria);
 
-        repository.save(livroParaAtualizar);
+        livroRepository.save(livroParaAtualizar);
     }
     @Test
     public void deletar(){
         UUID id = UUID.fromString("c2319c3b-4d51-43c2-bde4-44b2cc315961");
-        repository.deleteById(id);
+        livroRepository.deleteById(id);
     }
 
 
     @Test
     public void deletarCascade(){
         UUID id = UUID.fromString("eace85d9-9cc9-44f5-a05a-4941fd04c1df");
-        repository.deleteById(id);
+        livroRepository.deleteById(id);
     }
 
     @Test
     @Transactional // a do spring,abriu uma janela para fazer operações do banco e so vai fechar a execu do method
     public void  buscarLivroTest(){
         UUID id = UUID.fromString("74034080-f07f-4f0a-a2ba-87c3a72baef9");
-        Livro livro = repository.findById(id).orElse(null);
+        Livro livro = livroRepository.findById(id).orElse(null);
         System.out.println("Livro: ");
         System.out.println(livro.getTitulo());
 
         System.out.println("Autor: ");
         System.out.println(livro.getAutor().getNome());
     }
+
+    @Test
+    void perquisarPorTituloTest(){
+        List<Livro> lista = livroRepository.findByTitulo("O roubo da casa assombrada");
+        lista.forEach(System.out::println);
+
+    }
+
+    @Test
+    void pesquisarPorISBNTest(){
+        List<Livro> listaIsbn = livroRepository.findByIsbn("20847-84874");
+        //System.out.println("Livro achado de acordo com ISBN: "+ listaIsbn); - se fosse por objeto,mas vou fazer por lista
+        listaIsbn.forEach(System.out::println);
+    }
+
+    @Test
+    void pesquisarPorTituloEPreco(){
+        String titulo = "O roubo da casa assombrada";
+        BigDecimal preco = BigDecimal.valueOf(650);
+
+        List<Livro> listaIsbn = livroRepository.findByTituloAndPreco(titulo, preco);
+        listaIsbn.forEach(System.out::println);
+    }
+    @Test
+    void listarLivrosComQueryJPQL(){
+        var resultado = livroRepository.listarTodosOrdenadoPorTituloAndPreco();
+        resultado.forEach(System.out::println);
+    }
+
+    @Test
+    void listarAutoresDoLivroComQueryJPQL(){
+        var resultado = livroRepository.listarAutoresDosLivros();
+        resultado.forEach(System.out::println);
+    }
+    @Test
+    void listarTitulosNaoRepetidosComQueryJPQL(){
+        var resultado = livroRepository.listarNomesDiferentesLivros();
+        resultado.forEach(System.out::println);
+    }
+
+    @Test
+    void listarGenerosDeLivrosAutoresBrasileirosComQueryJPQL(){
+        var resultado = livroRepository.listarGenerosAutoresBrasileiros();
+        resultado.forEach(System.out::println);
+    }
+
+
 }
